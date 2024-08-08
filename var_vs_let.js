2e92e8035954f4c31528f962dd42549d466f2a72
Vitaliy Rabosh <rabosh@gmail.com>
2024-08-09 02:08:45 +0300

    Create var_vs_let.js

---
+++ var_vs_let.js
@@ -0,0 +1,21 @@
+var f = (y) => a*y;
+const eb = 1000000000;
+
+console.time('var for time')
+{
+     // var в этом случае работает в 10 раз медленей чем let.
+     // потомучто учитывается глобальное обращение к функции f(), где используется a
+    var a = 0;
+    for(var j = 0; j < eb; j++){
+        a = j**2 * 0.1;
+    }
+}
+console.timeEnd('var for time');
+console.time('let for time')
+{
+    let a = 0;
+    for(let j = 0; j < eb; j++){
+        a = j**2 * 0.1;
+    }
+}
+console.timeEnd('let for time');
